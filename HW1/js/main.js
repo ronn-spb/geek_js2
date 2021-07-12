95a8799364ec01182295d981563408d1dcb7a709
Ronnspb <shooter12@mail.ru>
2021-07-12 19:07:01 +0300

    modifed display products added delete to cart

--- HW1/js/main.js
+++ HW1/js/main.js
@@ -47,35 +47,58 @@ let pageProducts = [
         name: "Mango People T-shirt",
     }
 ];
+const catalogItems = document.getElementsByClassName("catalog_items");
 
-let itemCardHtml = '<div class="item_card">\
+
+
+
+
+
+let itemOutput = "";
+function displayProductCard({ id, price, name, img }) {
+    let itemCardHtml = `<div class="item_card">\
     <div class="overlay_btn">\
-    <div class="add_to_card flex" id="%id%" onclick="addToCart(this)">\
+    <div class="add_to_card flex" id="${id}" onclick="addToCart(this)">\
         <img src="img/card.svg" alt="">\
                 Add to Cart\
     </div>\
 </div>\
 <a href="singlepage.html">\
     <div class="item_crd_img">\
-        <img src="img/%img%" alt="Mango  People  T-shirt">\
+        <img src="img/${img}" alt="Mango  People  T-shirt">\
         <div class="overlay">\
         </div>\
     </div>\
     <div class="itemcard_info">\
-        <div class="item_name">%name%</div>\
-        <div class="item_price">$%price%</div>\
+        <div class="item_name">${name}</div>\
+        <div class="item_price">$ ${price}</div>\
     </div>\
 </a>\
-</div>'
-let itemMiniCart = '\
-<div class="cart_item flex flex_btw">\
+</div>`
+    return itemCardHtml
+}
+function displayItemsCards(list = pageProducts) {
+
+
+    let catalogItems = document.getElementsByClassName("catalog_items");
+    let itemOutput = '';
+    list.map(item => {
+        itemOutput += displayProductCard(item);
+    }
+    );
+    catalogItems[0].insertAdjacentHTML("afterbegin", itemOutput);
+
+}
+function renderCartItem({ name, img, price, id, quantity }) {
+    let itemMiniCart = `\
+<div class="cart_item flex flex_btw" >\
 <div class="flex">\
     <div class="cart_item_img">\
-        <img src="img/%img%" alt="cartitem">\
+        <img src="img/${img}" alt="cartitem">\
     </div>\
     <div class="cat_descr">\
         <div class="cart_item_name">\
-           %name%\
+           ${name}\
         </div>\
         <div class="cart_item_stars">\
             <i class="fas fa-star"></i>\
@@ -85,42 +108,28 @@ let itemMiniCart = '\
             <i class="fas fa-star-half-alt"></i>\
         </div>\
         <div class="cart_item_price">\
-            %quantity% x $%price%\
+            ${quantity} x $ ${price}\
         </div>\
     </div>\
 </div>\
-<div class="cart_delete_btn flex">\
-    <i class="fas fa-times-circle"></i>\
+<div class="cart_delete_btn flex" >\
+    <i class="fas fa-times-circle" id="${id}" onclick="deleteItemCart(this)"></i>\
 </div>\
 </div>\
-<div class="browse_decor"></div>'
-
-
-let itemOutput = "";
-
-function displayItemsCard() {
-
-    let catalogItems = document.getElementsByClassName("catalog_items");
-    console.log(catalogItems);
-    let itemOutput = "";
-    for (let i = 0; i < pageProducts.length; i++) {
-        itemOutput += (itemCardHtml.replace(/%name%/gi, pageProducts[i].name).replace(/%img%/gi, pageProducts[i].img).replace(/%price%/gi, pageProducts[i].price).replace(/%id%/gi, pageProducts[i].id));
-
-    }
-    catalogItems[0].insertAdjacentHTML("afterbegin", itemOutput);
-
+<div class="browse_decor"></div>`;
+    return itemMiniCart;
 }
-function updateCart() {
+function updateCart(list = cart) {
     let lvCart = document.getElementsByClassName("live_cart")[0];
 
     lvCart.style.display = "block";
     lvCart.innerText = cart.length + "";
     let cartItemList = document.getElementsByClassName("cart_item_list")[0];
-    //cartItemList.insertAdjacentHTML("beforeend", itemMiniCart);
+
     itemOutput = "";
-    for (let i = 0; i < cart.length; i++) {
-        itemOutput += (itemMiniCart.replace(/%name%/gi, cart[i].name).replace(/%img%/gi, cart[i].img).replace(/%price%/gi, cart[i].price).replace(/%id%/gi, cart[i].id)).replace(/%quantity%/gi, cart[i].quantity);
-    }
+    list.map(item => {
+        itemOutput += renderCartItem(item)
+    })
     cartItemList.innerHTML = itemOutput;
     let cartTotalEm = document.getElementsByClassName("cart_total_amount")[0];
     cartTotalEm.innerText = "$" + cartGetTotal();
@@ -143,7 +152,13 @@ function addToCart(e) {
             price: productObj.price
         });
     }
-    console.log(cart);
+
+    updateCart();
+}
+function deleteItemCart(e) {
+    cart = cart.filter((item) => {
+        return (item.id != e.id)
+    })
     updateCart();
 }
 function cartGetTotal() {
@@ -154,7 +169,8 @@ function cartGetTotal() {
     return total;
 }
 
-displayItemsCard();
+displayItemsCards();
+updateCart();
 
 
 
