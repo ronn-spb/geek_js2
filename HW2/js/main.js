419ad9dff8aff5481b0eb088f20a842c2112b197
Ronnspb <shooter12@mail.ru>
2021-07-15 13:29:02 +0300

    cart class add

--- HW2/js/main.js
+++ HW2/js/main.js
@@ -1,5 +1,87 @@
-let cart = [];
-// class cart 
+// let cart = [];
+class cart {
+    constructor() {
+        this.numGoods = 0;
+        this.cartList = [];
+    }
+    addToCart(e) {
+        let indexInCart = this.cartList.findIndex(product => product.id === e.id);
+
+        if (indexInCart != -1) {
+            cart[indexInCart].quantity += 1;
+        } else {
+            let productObj = pageProducts.find(product => product.id === e.id);
+
+            this.cartList.push({
+                id: productObj.id,
+                name: productObj.name,
+                img: productObj.img,
+                quantity: 1,
+                price: productObj.price
+            });
+        }
+
+        this.updateCart();
+    }
+    updateCart(list = this.cartList) {
+        let lvCart = document.getElementsByClassName("live_cart")[0];
+
+        lvCart.style.display = "block";
+        lvCart.innerText = list.length + "";
+        let cartItemList = document.getElementsByClassName("cart_item_list")[0];
+
+        itemOutput = "";
+        list.map(item => {
+            itemOutput += this.renderCartItem(item)
+        })
+        cartItemList.innerHTML = itemOutput;
+        let cartTotalEm = document.getElementsByClassName("cart_total_amount")[0];
+        cartTotalEm.innerText = "$" + this.cartGetTotal();
+    }
+    renderCartItem({ name, img, price, id, quantity }) {
+        let itemMiniCart = `\
+    <div class="cart_item flex flex_btw" >\
+    <div class="flex">\
+        <div class="cart_item_img">\
+            <img src="img/${img}" alt="cartitem">\
+        </div>\
+        <div class="cat_descr">\
+            <div class="cart_item_name">\
+               ${name}\
+            </div>\
+            <div class="cart_item_stars">\
+                <i class="fas fa-star"></i>\
+                <i class="fas fa-star"></i>\
+                <i class="fas fa-star"></i>\
+                <i class="fas fa-star"></i>\
+                <i class="fas fa-star-half-alt"></i>\
+            </div>\
+            <div class="cart_item_price">\
+                ${quantity} x $ ${price}\
+            </div>\
+        </div>\
+    </div>\
+    <div class="cart_delete_btn flex" >\
+        <i class="fas fa-times-circle" id="${id}" onclick="pageCart.deleteItemCart(this)"></i>\
+    </div>\
+    </div>\
+    <div class="browse_decor"></div>`;
+        return itemMiniCart;
+    }
+    deleteItemCart(e) {
+        this.cartList = this.cartList.filter((item) => {
+            return (item.id != e.id)
+        })
+        this.updateCart();
+    }
+    cartGetTotal() {
+        let total = 0;
+        for (let i = 0; i < this.cartList.length; i++) {
+            total += this.cartList[i].quantity * this.cartList[i].price;
+        }
+        return total;
+    }
+}
 let pageProducts = [
     {
         id: "1",
@@ -59,7 +141,7 @@ let itemOutput = "";
 function displayProductCard({ id, price, name, img }) {
     let itemCardHtml = `<div class="item_card">\
     <div class="overlay_btn">\
-    <div class="add_to_card flex" id="${id}" onclick="addToCart(this)">\
+    <div class="add_to_card flex" id="${id}" onclick="pageCart.addToCart(this)">\
         <img src="img/card.svg" alt="">\
                 Add to Cart\
     </div>\
@@ -78,6 +160,7 @@ function displayProductCard({ id, price, name, img }) {
 </div>`
     return itemCardHtml
 }
+
 function displayItemsCards(list = pageProducts) {
 
 
@@ -90,88 +173,10 @@ function displayItemsCards(list = pageProducts) {
     catalogItems[0].insertAdjacentHTML("afterbegin", itemOutput);
 
 }
-function renderCartItem({ name, img, price, id, quantity }) {
-    let itemMiniCart = `\
-<div class="cart_item flex flex_btw" >\
-<div class="flex">\
-    <div class="cart_item_img">\
-        <img src="img/${img}" alt="cartitem">\
-    </div>\
-    <div class="cat_descr">\
-        <div class="cart_item_name">\
-           ${name}\
-        </div>\
-        <div class="cart_item_stars">\
-            <i class="fas fa-star"></i>\
-            <i class="fas fa-star"></i>\
-            <i class="fas fa-star"></i>\
-            <i class="fas fa-star"></i>\
-            <i class="fas fa-star-half-alt"></i>\
-        </div>\
-        <div class="cart_item_price">\
-            ${quantity} x $ ${price}\
-        </div>\
-    </div>\
-</div>\
-<div class="cart_delete_btn flex" >\
-    <i class="fas fa-times-circle" id="${id}" onclick="deleteItemCart(this)"></i>\
-</div>\
-</div>\
-<div class="browse_decor"></div>`;
-    return itemMiniCart;
-}
-function updateCart(list = cart) {
-    let lvCart = document.getElementsByClassName("live_cart")[0];
 
-    lvCart.style.display = "block";
-    lvCart.innerText = cart.length + "";
-    let cartItemList = document.getElementsByClassName("cart_item_list")[0];
-
-    itemOutput = "";
-    list.map(item => {
-        itemOutput += renderCartItem(item)
-    })
-    cartItemList.innerHTML = itemOutput;
-    let cartTotalEm = document.getElementsByClassName("cart_total_amount")[0];
-    cartTotalEm.innerText = "$" + cartGetTotal();
-}
-
-
-function addToCart(e) {
-    let indexInCart = cart.findIndex(product => product.id === e.id);
-    console.log(indexInCart);
-    if (indexInCart != -1) {
-        cart[indexInCart].quantity += 1;
-    } else {
-        let productObj = pageProducts.find(product => product.id === e.id);
-
-        cart.push({
-            id: productObj.id,
-            name: productObj.name,
-            img: productObj.img,
-            quantity: 1,
-            price: productObj.price
-        });
-    }
-
-    updateCart();
-}
-function deleteItemCart(e) {
-    cart = cart.filter((item) => {
-        return (item.id != e.id)
-    })
-    updateCart();
-}
-function cartGetTotal() {
-    let total = 0;
-    for (let i = 0; i < cart.length; i++) {
-        total += cart[i].quantity * cart[i].price;
-    }
-    return total;
-}
-
+pageCart = new cart();
 displayItemsCards();
-updateCart();
+pageCart.updateCart();
 
 
 
